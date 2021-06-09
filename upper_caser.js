6d816172b4753f77fd907bd637edb21bedea81e3
Dephrase <davidjf123@gmail.com>
2021-06-09 16:41:28 +0100

    Finished UpperCaseFunction

--- upper_caser.js
+++ upper_caser.js
@@ -1,7 +1,13 @@
 const UpperCaser = function (words) {
+    this.words = words;
 }
 
 UpperCaser.prototype.toUpperCase = function () {
+    let upperCaseWords = this.words.map((word) => {
+        return word.toUpperCase()
+    })
+    return upperCaseWords
+
 
 }
 
